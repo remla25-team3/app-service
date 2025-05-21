8568b4114f8f599628ebd19346766dea59037260
LvHTUD <33719406+LvHTUD@users.noreply.github.com>
2025-05-21 20:36:37 +0200

    chore:Add exporter for Prometheus metrics (#6)
    * Add metrics endpoint; add counter metrics
    
    * Add metrics endpoint documentation
    
    * feat:Add integration with prometheus_flask_explorer
    
    * fix:Remove /metrics endpoint because exporter exposes by default
    
    * fix:Remove explicit labels in metrics observation lines
    
    * feat:Add gauge metric for currently active requests
    
    * fix:Temporarily disable some metrics due to error
    
    ---------
    
    Co-authored-by: Lennard van Hal <levanhal@tudelft.nl>

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 import os
 import requests
-from flask import Flask, request
+from flask import Flask, request, Response
+from prometheus_flask_exporter import PrometheusMetrics
 from flasgger import Swagger
-from libversion.version_util import VersionUtil
+from libversion import version_util
 
 app = Flask(__name__)
 swagger = Swagger(app)
+metrics = PrometheusMetrics(app)
 
 # Fetch URL/port to model-service from environment variables (service name from docker-compose)
 model_service_url = os.getenv('MODEL_SERVICE_URL', default='model-service')
 model_service_port = os.getenv('MODEL_SERVICE_PORT', default='8081')
+
+
+## Metrics
+
+# General info
+metrics.info('app_info', 'Application info', version=version_util.VersionUtil.get_version())
+
+review_input_length = metrics.histogram(
+	'input_length_vs_prediction', 'Histogram of review input lengths verus predictions',
+	labels={'prediction': lambda response: response.text}
+)
+
+prediction_count_by_type = metrics.counter(
+	'prediction_count_by_type', 'Number of predictions by type',
+	labels={'prediction': lambda response: response.text}
+)
+
+active_prediction_requests = metrics.gauge(
+	'active_prediction_requests', 'Number of requests being processed at a time'
+)
 
 
 @app.route('/get-prediction', methods=['POST'])
@@ -30,24 +52,38 @@ def get_prediction():
 		500:
 			description: Prediction could not be fetched from `model-service`.
 	"""
+	# Keep track of currently active requests
+	# active_prediction_requests.inc()
+
 	try:
-		msg = request.get_json()
-	except Exception:
-		return 'Payload should be a JSON object with "review" as key', 400
+		try:
+			msg = request.get_json()
+		except Exception:
+			return 'Payload should be a JSON object with "review" as key', 400
 
-	if not 'review' in msg:
-		return 'JSON payload should contain "review" key', 400
+		if not 'review' in msg:
+			return 'JSON payload should contain "review" key', 400
 
-	req_url = f'http://{model_service_url}:{model_service_port}/predict'
-	try:
-		model_service_response = requests.post(req_url, json=msg)
-	except Exception:
-		return f'Could not connect to model-service', 500
+		req_url = f'http://{model_service_url}:{model_service_port}/predict'
+		try:
+			model_service_response = requests.post(req_url, json=msg)
+		except Exception:
+			return f'Could not connect to model-service', 500
 
-	if model_service_response.ok:
-		return model_service_response.text
+		if model_service_response.ok:
+			# If we got a response from `model-service`, feed the input's length to the metrics
+			review_input_length.observe(len(msg['review']))
 
-	return 'Could not fetch prediction', 500
+			# Also increment prediction count by type
+			# prediction_count_by_type.inc()
+
+			return model_service_response.text
+
+		return 'Could not fetch prediction', 500
+	finally:
+		# Request was finished; decrease number of active requests
+		# active_prediction_requests.dec()
+		pass
 
 
 @app.route('/update-prediction', methods=['POST'])
@@ -79,9 +115,6 @@ def update_prediction():
 	if not 'label' in msg:
 		return 'JSON payload should contain "label" key', 400
 
-	# Here, we would want to send a request to model-service,
-	# but for testing purposes we don't.
-	# req_url = f'http://{model_service_url}:{model_service_port}/update-prediction'
 	return f'Review label was successfully updated to {msg['label']}'
 
 
@@ -97,9 +130,10 @@ def get_lib_version():
 			description: Version could not be retrieved.
 	"""
 	try:
-		return VersionUtil.get_version()
+		return version_util.VersionUtil.get_version()
 	except Exception as e:
 		print(e)
 		return "Version not found", 500
+
 
 app.run(host="0.0.0.0", port=5000)
